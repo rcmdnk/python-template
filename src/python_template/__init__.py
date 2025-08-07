c16ddc2fde33849221745310b25befe965ba18e4
rcmdnk <rcmdnk@gmail.com>
2025-08-07 22:53:12 +0900

    refactor: Simplify version handling by using a dedicated version.py module

--- src/python_template/__init__.py
+++ src/python_template/__init__.py
@@ -3,7 +3,7 @@ __all__ = ['__version__']
 
 def __getattr__(name: str) -> str:
     if name == '__version__':
-        from .__version__ import __version__
+        from .version import __version__
 
         return __version__
     msg = f'module {__name__} has no attribute {name}'
--- src/python_template/__version__.py
+++
@@ -1,7 +0,0 @@
-def __getattr__(name: str) -> str:
-    if name == '__version__':
-        import importlib.metadata
-
-        return importlib.metadata.version(__package__ or __name__)
-    msg = f'module {__name__} has no attribute {name}'
-    raise AttributeError(msg)
